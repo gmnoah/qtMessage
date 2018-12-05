c80e6183621d4158849e9899f5e9e245afbb2785
gmnoah <31761064+gmnoah@users.noreply.github.com>
2018-12-05 16:28:07 +0800

    Add files via upload

---
+++ git_messageGlable.pro
@@ -0,0 +1,12 @@
+QT += core
+QT -= gui
+
+CONFIG += c++11
+
+TARGET = git_messageGlable
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include <QCoreApplication>
+#include <QMutex>
+#include <QFile>
+#include <QDateTime>
+#include <QDir>
+#include <QTextStream>
+#include <QDebug>
+void messageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    static QMutex mutex;
+    mutex.lock();
+
+    QString  currentDateTimeString = QDateTime::currentDateTime().toLocalTime().toString(Qt::DefaultLocaleLongDate);
+    QString ctxInfo = context.file + QLatin1Char(':') + QString::number(context.line) + QLatin1Char(':') + context.function;
+    QString message = currentDateTimeString + QLatin1Char(' ')+ msg + QLatin1Char('[') + ctxInfo + QLatin1Char(']');
+
+    static QString localDirPath = QDir::toNativeSeparators(qApp->applicationDirPath())/*QDir::separator() + QLatin1String("log")*/;
+    static QString fileName = localDirPath + QDir::separator() + qApp->applicationName();
+
+    QFile file(fileName);
+    if (file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append))
+    {
+        if (file.size() >= Q_INT64_C(10240 * 128))//1M*128
+            file.resize(0);
+
+        QTextStream in(&file);
+        in << message << endl;
+    }
+    mutex.unlock();
+
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    qInstallMessageHandler(messageOutput);
+    qDebug() <<"something is happen";
+    return a.exec();
+}
